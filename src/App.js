4f2e8d245022f4ab6228e7949a84418d97fb7814
Francisco Sabino <fdasabino@gmail.com>
2023-06-12 13:27:24 +0200

    updated basket slide path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
 import Hero from "./components/Hero/Hero";
-import Slideshow from "./components/Hero/slideshow/Slideshow";
 import About from "./components/about/About";
 import FooterSection from "./components/footer/Footer";
 import Globe from "./components/globe/Globe";
 import Navbar from "./components/navbar/Navbar";
+import Slideshow from "./components/slideshow/Slideshow";
 import "./global.css";
 
 function App() {
