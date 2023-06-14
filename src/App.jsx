810eaca3e8b20c5be9d3ac25d57678425f19a0d8
Munniza <you@example.com>
2023-06-14 19:25:40 +0200

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import About from "./components/about/About";
-import Hero from "./components/hero/Hero";
+import Hero from "./components/Hero/Hero";
 import Slideshow from "./components/slideshow/Slideshow";
 
 function App() {
